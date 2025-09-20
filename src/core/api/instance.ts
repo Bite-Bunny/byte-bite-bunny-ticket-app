dc7cb05053391491d177c35aad7ce8c90b26f8ac
Narek Hovhannisyan <hnarek2005@gmail.com>
2025-09-20 22:42:59 +0400

    feat: added init data to api client

--- src/core/api/instance.ts
+++ src/core/api/instance.ts
@@ -1,8 +1,14 @@
 import axios from 'axios'
 import { axiosHeaders, axiosTimeout, baseURL } from './config'
+import { retrieveRawInitData } from '@telegram-apps/sdk-react'
+
+const initDataRaw: string | undefined = retrieveRawInitData()
 
 export const apiClient = axios.create({
   baseURL: baseURL,
-  headers: axiosHeaders,
+  headers: {
+    ...axiosHeaders,
+    Authorization: initDataRaw,
+  },
   timeout: axiosTimeout,
 })
