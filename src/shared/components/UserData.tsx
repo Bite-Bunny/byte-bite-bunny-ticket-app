31a1b66e2352bc15dce7987e2c149745b6ac9900
Narek Hovhannisyan <hnarek2005@gmail.com>
2025-09-20 23:26:33 +0400

    feat: added copy raw data

--- src/shared/components/UserData.tsx
+++ src/shared/components/UserData.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import { useRawInitData } from '@telegram-apps/sdk-react'
 import useMe from '../hooks/useMe'
 
 export default function UserData() {
   const { data: me, isLoading, error } = useMe()
+  const rawData = useRawInitData()
 
   if (isLoading) {
     return <div>Loading user data...</div>
@@ -18,6 +20,19 @@ export default function UserData() {
       <div>Welcome to Bite Bunny!</div>
       <div>
         <div>Hi, {me?.user.first_name}</div>
+      </div>
+
+      <div>
+        <pre>{JSON.stringify(rawData, null, 2)}</pre>
+
+        <button
+          className="bg-blue-500 text-white p-2 rounded-md"
+          onClick={() => {
+            navigator.clipboard.writeText(rawData || '')
+          }}
+        >
+          Copy raw data
+        </button>
       </div>
 
       <pre>{JSON.stringify(me, null, 2)}</pre>
